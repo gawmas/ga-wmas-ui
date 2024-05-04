8af75a1e9d58712bb4ca086ff5c773a73117f82c
Dave Okey <david.j.okey@gmail.com>
2024-05-04 15:56:35 -0400

    Cleanup; admin api split

--- src/app/_shared/services/admin.service.ts
+++ src/app/_shared/services/admin.service.ts
@@ -9,21 +9,22 @@ export class AdminService {
 
   http = inject(HttpClient);
   private endpoint = `${env.API_URL}`;
+  private adminEndpoint = `${env.ADMIN_API_URL}`;
 
   getHunt(id: string): Observable<Hunt> {
     return this.http.get<Hunt>(`${this.endpoint}/hunt/${id}`);
   }
 
   updateHunt(hunt: Hunt): Observable<Hunt> {
-    return this.http.put<Hunt>(`${this.endpoint}/hunt/${hunt.id}`, hunt);
+    return this.http.put<Hunt>(`${this.adminEndpoint}/hunt/${hunt.id}`, hunt);
   }
 
   getAdminWmas(): Observable<Wma[]> {
-    return this.http.get<Wma[]>(`${this.endpoint}/adminWmas`);
+    return this.http.get<Wma[]>(`${this.adminEndpoint}/adminWmas`);
   }
 
   updateWma(wma: Wma): Observable<Wma> {
-    return this.http.put<Wma>(`${this.endpoint}/wma/${wma.id}`, wma);
+    return this.http.put<Wma>(`${this.adminEndpoint}/wma/${wma.id}`, wma);
   }
 
 }
