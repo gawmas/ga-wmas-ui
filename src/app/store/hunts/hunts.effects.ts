7bfa683d60d0cfdde5756bbf03001a9d58e55b63
Dave Okey <david.j.okey@gmail.com>
2025-03-16 09:16:51 -0400

    Intermediate ngrx update > v18

--- src/app/store/hunts/hunts.effects.ts
+++ src/app/store/hunts/hunts.effects.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
-import { Actions, concatLatestFrom, createEffect, ofType } from '@ngrx/effects';
+import { Actions, createEffect, ofType } from '@ngrx/effects';
+import { concatLatestFrom } from '@ngrx/operators';
 import { HuntService } from '@services';
 import { Store } from '@ngrx/store';
 import { withLatestFrom, map, switchMap, catchError, of, tap, debounceTime, exhaustMap } from 'rxjs';
