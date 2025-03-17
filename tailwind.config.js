7cc191b8a743e2da7497074408e02fc609dc3fdc
Dave Okey <david.j.okey@gmail.com>
2025-03-17 11:53:26 -0400

    Tailwind config update

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  mode: 'jit',
   content: [
-    "./src/**/*.{html,ts,scss}",
+    "./src/**/*.{html,ts,scss,css}",
   ],
   theme: {
     extend: {
